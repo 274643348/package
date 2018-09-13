4cc62eb2e51667bf7faa8ed8b9bb265f933ff00b
XiaoYan <liujingyan@congmingpai.com>
2018-09-13 12:04:01 +0800

    中文字符检查

--- packages/hello-world/main.js
+++ packages/hello-world/main.js
@@ -18,6 +18,69 @@ module.exports = {
     },
     'cc' () {
         Editor.log('上海市');
-      }
+      },
+    'queryAssets'(){
+        const assetdb = Editor.assetdb;
+        Editor.clearLog();
+        Editor.log("start----------------------------------");
+        let fileCount = 0;
+        assetdb.queryAssets("db:assets/*","",(err,results)=>{
+          if(err){
+            Editor.error(err);
+          }else{
+            for (const it of results) {
+              fileCount++;
+              Editor.log(it.path + "---count---" +fileCount);
+            }
+          }
+        });
+      },
+      'queryAssetsFiel'(){
+          const assetdb = Editor.assetdb;
+          Editor.clearLog();
+          Editor.log("start----------------------------------");
+          let fileCount = 0;
+          assetdb.queryAssets("db:assets/*","",(err,results)=>{
+            if(err){
+              Editor.error(err);
+            }else{
+              for (const it of results) {
+                if (!it.isSubAsset) {
+                  fileCount++;
+                  Editor.log(it.path + "---count---" +fileCount);
+                }
+              }
+            }
+          })
+        },
+        'checkChina'(){
+            const assetdb = Editor.assetdb;
+            Editor.clearLog();
+            Editor.log("start----------------------------------");
+            let fileCount = 0;
+            assetdb.queryAssets("db:assets/*","",(err,results)=>{
+              if(err){
+                Editor.error(err);
+              }else{
+                for (const it of results) {
+                  const result = this.hasChina(it.path);
+                  if (!it.isSubAsset && result) {
+                    fileCount++;
+                    Editor.error(it.path + "---count---" +fileCount);
+                  }
+                }
+              }
+            })
+          }
   },
+
+
+  hasChina(str) {
+    if (escape(str).indexOf("%u") > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 };
